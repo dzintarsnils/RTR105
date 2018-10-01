926eaeb1ae41490a5c162107e3ce22bd9dcf8bde
Live session user <dzintarsnilselsts@gmail.com>
2018-10-01 15:00:56 +0000

    20181001_18_00

---
+++ test_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+#http://www.cplusplus.com/reference/sctdio/printf/
+
+a = 10
+b = 1.56
+c = 'A'
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+a = 6969
+b = 69.6969707
+c = 'A'
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+
